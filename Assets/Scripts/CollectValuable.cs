1310e782679933723d1865db87d7cb2e062cda6f
Razvan <dracopol.razvan@gmail.com>
2025-03-13 14:31:05 +0200

    add CallectValuable and PerformTransaction sounds

--- Assets/Scripts/CollectValuable.cs
+++ Assets/Scripts/CollectValuable.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class pickup : MonoBehaviour
 {
+    [SerializeField] AudioClip sound;
+
+    AudioSource audioSource;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Valuable"))
         {
             Destroy(other.gameObject);
+            audioSource.PlayOneShot(sound);
         }
     }
 }
